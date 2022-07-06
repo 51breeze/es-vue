2c3c912c14d6ae91d7b1f9c4ca5cb2196f210947
Jun Ye <664371281@qq.com>
2022-07-06 22:45:34 +0800

    v1

--- test/build/web/components/KeepAlive.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © 2017 EaseScript All rights reserved.
- * Released under the MIT license
- * https://github.com/51breeze/EaseScript
- * @author Jun Ye <664371281@qq.com>
- */
-///__REFS__
-var KeepAlive = Vue.component('KeepAlive');
-module.exports=KeepAlive;
--- test/build/web/ui/Select.js
+++
@@ -1,4 +0,0 @@
-require("./../../../../node_modules/element-ui/lib/theme-chalk/select.css");
-require("./../../../../node_modules/element-ui/lib/theme-chalk/icon.css");
-const Select = require("element-ui/packages/select");
-module.exports=web_ui_Select;
