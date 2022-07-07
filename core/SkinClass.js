4ed5ce77ebd9b441b9b599ce38a4b3f7a3405530
Jun Ye <664371281@qq.com>
2022-07-07 22:25:05 +0800

    fix

--- core/SkinClass.js
+++ core/SkinClass.js
@@ -1,8 +1,17 @@
 
 const Core = require("./Core.js");
-const Constant = Core.constant;
 const JSXClassBuilder = require("./JSXClassBuilder.js");
 class SkinClass extends JSXClassBuilder{
-
+    isActiveForModule(module, ctxModule){
+        if( module === this.module.inherit){
+            return true;
+        }
+        return super.isActiveForModule(module, ctxModule);
+    }
+    adjustConstructor(node, module){
+        if( !node.construct ){
+            node.construct = this.createDefaultConstructMethod(node, module, null, null, [this.createIdentifierNode('context')]);
+        }
+    }
 }
 module.exports = SkinClass;
